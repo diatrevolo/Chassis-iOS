78d06ede0a2c1f1fd5d518a8b29d2c1840d373e9
Roberto J. Osorio-Goenaga <rjo218@gmail.com>
2020-06-16 14:29:12 -0400

    Adds Apache license and modifies headers.

--- Chassis/CheckError.h
+++ Chassis/CheckError.h
@@ -3,7 +3,19 @@
 //  Chassis
 //
 //  Created by Roberto Osorio Goenaga on 6/16/20.
-//  Copyright © 2020 Roberto Osorio Goenaga. All rights reserved.
+//  Copyright © 2020 Roberto Osorio Goenaga.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #ifndef CheckError_h
