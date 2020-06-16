fd9f136dd8bfad11a19d2ac6714c360c3d48f1d2
Roberto J. Osorio-Goenaga <rjo218@gmail.com>
2020-06-16 13:32:36 -0400

    Initial Commit

---
+++ Chassis/Chassis.h
@@ -0,0 +1,19 @@
+//
+//  Chassis.h
+//  Chassis
+//
+//  Created by Roberto Osorio Goenaga on 6/16/20.
+//  Copyright © 2020 Roberto Osorio Goenaga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Chassis.
+FOUNDATION_EXPORT double ChassisVersionNumber;
+
+//! Project version string for Chassis.
+FOUNDATION_EXPORT const unsigned char ChassisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chassis/PublicHeader.h>
+
+
